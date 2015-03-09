993cc4c348733c1aab4b05667f7d679527f68d36
Rey <quit9909@icloud.com>
2015-03-09 21:07:13 +0900

    Initial Commit

---
+++ TechSeconds/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TechSeconds
+//
+//  Created by ReyMasuda on 2015/03/09.
+//  Copyright (c) 2015年 ReyMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TechSeconds/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TechSeconds
+//
+//  Created by ReyMasuda on 2015/03/09.
+//  Copyright (c) 2015年 ReyMasuda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
